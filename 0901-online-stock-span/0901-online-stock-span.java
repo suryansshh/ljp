31daa096acfc503443a17260b395941f387e2318
suryansh sisodia <suryanshsisodiaprog@gmail.com>
2024-03-19 01:12:03 +0530

    Time: 33 ms (35.46%), Space: 49.6 MB (93.28%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,21 @@
+
+class StockSpanner {
+    private Stack<Integer> prices;
+    private Stack<Integer> spans;
+
+    public StockSpanner() {
+        prices = new Stack<>();
+        spans = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            span += spans.pop();
+        }
+        prices.push(price);
+        spans.push(span);
+        return span;
+    }
+}
