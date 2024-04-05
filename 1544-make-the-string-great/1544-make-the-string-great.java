58e938d51a39c41ef03b893a0eabfc8502bba77a
suryansh sisodia <suryanshsisodiaprog@gmail.com>
2024-04-05 18:20:11 +0530

    Time: 1 ms (100%), Space: 41.5 MB (98.87%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String makeGood(String s) {
+        if (s.length() == 0 || s.length() == 1)
+            return s;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(s);
+
+        int i = 0;
+
+        while (i < sb.length() - 1)
+        {
+            char leftChar = sb.charAt(i);
+            char rightChar = sb.charAt(i + 1);
+
+            if (Math.abs(leftChar - rightChar) == 32)
+            {
+                sb.delete(i, i + 2);
+                if (i > 0)
+                    i--;
+            }
+            else
+            {
+                i++;
+            } 
+        }
+
+        return sb.toString();
+    }
+}
