a0b6b64679668aac315ed746a12d481bdae582c7
suryansh sisodia <suryanshsisodiaprog@gmail.com>
2024-06-12 13:16:44 +0530

    Time: 1 ms (21.51%), Space: 41.8 MB (42.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,23 +1,5 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // to sovle the array using selection sort
-       // int didswap = 0 ;
-        int n = nums.length;
-        for(int i = 0 ; i <n-1;i++){
-            int min = i ;
-            for(int j = i+1 ; j<n ;j++){
-                if(nums[j]<nums[min]) min = j ;
-                 
-              
-        }
-           swap(nums , i , min);
-          
-    }
-   
-}
-     public  void swap(int[] arr , int j , int min){
-        int temp = arr[j];
-        arr[j] = arr[min];
-        arr[min] = temp;
+        Arrays.sort(nums);
     }
 }
